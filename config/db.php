923ffe15c6f5e194146aebe7ea053ca6c0aac8dd
SergStas <sergstas0@gmail.com>
2022-04-05 21:51:06 +0500

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=26.235.133.51;port=6033;dbname=blog_db',
+    'username' => 'root',
+    'password' => '22334455',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
---
+++ modules/admin/views/default/index.php
@@ -0,0 +1,6 @@
+<div class="admin-default-index">
+
+<h1>
+    This is default page, redirect to articles to edit
+</h1>
+</div>
